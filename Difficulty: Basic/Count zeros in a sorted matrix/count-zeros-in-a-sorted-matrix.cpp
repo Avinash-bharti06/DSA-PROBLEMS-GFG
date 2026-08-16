7681fbf0b60e13cb356accf583e76aad10c03fa5
Avinash Bharti <avinashbharti3007@gmail.com>
2026-08-16 19:17:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count zeros in a sorted matrix/count-zeros-in-a-sorted-matrix.cpp
@@ -0,0 +1,16 @@
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+    int countZeros(vector<vector<int>>& mat) {
+        // code here
+        int n = mat.size();
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(mat[i][j]==0) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
